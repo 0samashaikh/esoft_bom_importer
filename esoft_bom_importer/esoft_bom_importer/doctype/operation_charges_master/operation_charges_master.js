720725fc267d1886a9dea54aa3fa61e7a24aaf95
Abdul Mannan Shaikh <abdul.shaikh@esoftech.com>
2025-06-23 11:29:55 +0000

    feat: new doctypes and customizations added

---
+++ esoft_bom_importer/esoft_bom_importer/doctype/operation_charges_master/operation_charges_master.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, shaikhosama504 and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Operation Charges Master", {
+// 	refresh(frm) {
+
+// 	},
+// });
